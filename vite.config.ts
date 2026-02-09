5b70b9a00a569575ebb6acae8c8b7b46f0985981
Franklin Shera <fshera96@gmail.com>
2026-02-10 01:42:02 +0300

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
+import { reactRouterHonoServer } from "react-router-hono-server/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { reactRouterHonoServer } from "react-router-hono-server/dev";
 
 export default defineConfig({
   plugins: [
