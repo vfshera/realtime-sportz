9eef7522ffab5066875cc3612714fbac870e082e
Franklin Shera <fshera96@gmail.com>
2026-02-10 18:04:27 +0300

    fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-export default defineConfig(
+export default defineConfig([
   {
     ignores: [
       "node_modules/",
@@ -116,4 +116,4 @@ export default defineConfig(
     },
   },
   eslintConfigPrettier,
-);
+]);
