b3f76738377ecb8f16bfd9e9add96fc5cbca9c9e
Franklin Shera <fshera96@gmail.com>
2026-02-10 16:49:05 +0300

    deny hono sever files from being imported

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,17 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { reactRouterHonoServer } from "react-router-hono-server/dev";
 import { defineConfig } from "vite";
+import { denyImports } from "vite-env-only";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    denyImports({
+      client: {
+        files: ["server/**/*"],
+      },
+    }),
     reactRouterHonoServer({ serverEntryPoint: "./server/index.ts" }),
     reactRouter(),
     tsconfigPaths(),
