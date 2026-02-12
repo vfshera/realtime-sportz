f0c783f2d2c928503519053f4d1035823cffcafd
Franklin Shera <fshera96@gmail.com>
2026-02-12 05:06:00 +0300

    update types

--- server/types.ts
+++ server/types.ts
@@ -1,14 +1,14 @@
 import type { DB } from "~/.server/db";
 import type { Env, PublicEnv } from "~/env.server";
 import type { RequestIdVariables } from "hono/request-id";
+import type { WSContext } from "hono/ws";
+import type { WebSocket as RawWebSocket } from "ws";
 
-export type SessionVariables = object;
-
-export type AppBindings = {
-  Variables: SessionVariables & RequestIdVariables;
+type AppBindings = {
+  Variables: RequestIdVariables;
 };
 
-export type BaseContext = SessionVariables & {
+type BaseContext = {
   appVersion: string;
   requestId: string;
   clientEnv: PublicEnv;
@@ -20,3 +20,14 @@ declare module "react-router" {
   // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   interface RouterContextProvider extends BaseContext {}
 }
+
+declare module "ws" {
+  interface WebSocket {
+    isAlive: boolean;
+    subscriptions: Set<string>;
+  }
+}
+
+type NodeWSContext = WSContext<RawWebSocket>;
+
+export type { RawWebSocket, AppBindings, BaseContext, NodeWSContext };
