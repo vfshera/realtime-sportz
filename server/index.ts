d72a548007483404164a4ea7190330ccea07ca00
Franklin Shera <fshera96@gmail.com>
2026-02-12 05:19:59 +0300

    add websocket handler

--- server/index.ts
+++ server/index.ts
@@ -3,11 +3,11 @@ import { db } from "~/.server/db";
 import { clientEnv, env } from "~/env.server";
 import { appContext } from "./context";
 import type { AppBindings } from "./types";
+import { createWSHandler } from "./websocket/handler";
+import type { Context } from "hono";
 import { requestId } from "hono/request-id";
-import type { WSContext } from "hono/ws";
+import type { WSEvents } from "hono/ws";
 import { createHonoServer } from "react-router-hono-server/node";
-
-const clients = new Set<WSContext>();
 
 export default await createHonoServer<AppBindings>({
   useWebSocket: true,
@@ -17,31 +17,12 @@ export default await createHonoServer<AppBindings>({
   configure: (app, { upgradeWebSocket }) => {
     app.get(
       "/ws",
-      upgradeWebSocket((c) => ({
-        onOpen(_, ws) {
-          console.log("New connection ⬆️");
-          clients.add(ws);
-        },
-        onMessage(event, ws) {
-          console.log("Context", c.req.header("Cookie"));
-          console.log("Event", event);
-          console.log(`Message from client: ${event.data}`);
-          // Broadcast to all clients except sender
-          clients.forEach((client) => {
-            if (client.readyState === 1) {
-              client.send(`${event.data}`);
-            }
-          });
-        },
-        onClose(_, ws) {
-          console.log("Connection closed");
-          clients.delete(ws);
-        },
-        onError(err, ws) {
-          console.error("WebSocket error:", err);
-          clients.delete(ws);
-        },
-      })),
+      upgradeWebSocket(
+        (c: Context<AppBindings>) =>
+          createWSHandler(
+            c,
+          ) as WSEvents /** app types not inferred correctly by 'upgradeWebSocket' */,
+      ),
     );
   },
   getLoadContext: (ctx, { build }) => {
---
+++ server/websocket/handler.ts
@@ -0,0 +1,35 @@
+import type { AppBindings, RawWebSocket } from "../types";
+import { pubsub } from "./pubsub";
+import type { Context } from "hono";
+import type { WSEvents } from "hono/ws";
+
+export function createWSHandler(
+  c: Context<AppBindings>,
+): WSEvents<RawWebSocket> {
+  return {
+    onOpen(_, ws) {
+      console.log("New connection ⬆️");
+
+      pubsub.addSocket(ws);
+    },
+
+    onMessage(event, ws) {
+      try {
+        const data = JSON.parse(event.data.toString());
+        console.log("Received message:", data);
+      } catch (err) {
+        console.error("Failed to parse message:", err);
+      }
+    },
+
+    onClose(_, ws) {
+      console.log("Connection closed");
+      pubsub.removeSocket(ws);
+    },
+
+    onError(err, ws) {
+      console.error("WebSocket error:", err);
+      ws.raw?.terminate();
+    },
+  };
+}
