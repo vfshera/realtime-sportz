7dd8f05d068eb683943f91cc7a2da46d60e2d8e5
Franklin Shera <fshera96@gmail.com>
2026-02-15 16:05:42 +0300

    add date utils

---
+++ app/utils/date.ts
@@ -0,0 +1,32 @@
+export function getTodayUtcRange() {
+  const now = new Date();
+
+  const start = new Date(
+    Date.UTC(
+      now.getUTCFullYear(),
+      now.getUTCMonth(),
+      now.getUTCDate(),
+      0,
+      0,
+      0,
+      0,
+    ),
+  );
+
+  const end = new Date(
+    Date.UTC(
+      now.getUTCFullYear(),
+      now.getUTCMonth(),
+      now.getUTCDate() + 1,
+      0,
+      0,
+      0,
+      0,
+    ),
+  );
+
+  return {
+    startOfToday: start.getTime(),
+    endOfToday: end.getTime(),
+  };
+}
