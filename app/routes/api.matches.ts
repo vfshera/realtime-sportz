5e422d171375d040fa2bcf216a1686cf1a1ebd23
Franklin Shera <fshera96@gmail.com>
2026-02-20 14:26:33 +0300

    update matches api

--- app/routes/api.matches.ts
+++ app/routes/api.matches.ts
@@ -1,14 +1,14 @@
 import type { Route } from "./+types/api.matches";
 import { data } from "react-router";
 import matches, { type NewMatch } from "~/.server/db/schema/matches";
+import { requireJson } from "~/utils/api";
 import { createMatchSchema } from "~/validations/matches";
 import { appContext } from "$/server/context";
 
 export async function action({ request, context }: Route.ActionArgs) {
-  const raw = (await request.json()) as {
-    startTime: string;
-    endTime: string;
-  };
+  requireJson(request);
+
+  const raw = await request.json();
 
   const res = createMatchSchema.safeParse({
     ...raw,
@@ -24,20 +24,18 @@ export async function action({ request, context }: Route.ActionArgs) {
       },
       {
         status: 422,
-        statusText: "Validation Error",
-        headers: { "Content-Type": "application/json" },
       },
     );
   }
 
   const { db } = context.get(appContext);
 
-  const newMatch = await db
+  const [newMatch] = await db
     .insert(matches)
     .values(res.data as NewMatch)
     .returning();
 
-  return data({ ok: true, data: newMatch });
+  return data({ ok: true, data: newMatch }, { status: 201 });
 }
 
 export async function loader({ context }: Route.LoaderArgs) {
@@ -45,5 +43,5 @@ export async function loader({ context }: Route.LoaderArgs) {
 
   const allMatches = await db.query.matches.findMany();
 
-  return data({ data: allMatches, ok: true });
+  return data({ data: allMatches });
 }
