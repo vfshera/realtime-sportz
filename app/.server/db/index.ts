8ac05deabcda2ff8af14ae5a4d73234bb9bc3211
Franklin Shera <fshera96@gmail.com>
2026-02-25 01:21:46 +0300

    feat(db): enable libSQL WAL mode

--- app/.server/db/index.ts
+++ app/.server/db/index.ts
@@ -1,7 +1,10 @@
 import { env } from "~/env.server";
 import * as schema from "./schema";
+import { sql } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/libsql";
 
 export const db = drizzle({ connection: env.DB_FILE_NAME, schema });
+
+await db.run(sql`PRAGMA journal_mode = WAL;`);
 
 export type DB = typeof db;
